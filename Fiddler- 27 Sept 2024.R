08759a3bfcd47fcaf777cf2e3cce324a8a29f54a
RNiederman <rob.niederman@trssllc.com>
2024-09-29 11:15:25 -0400

    29Sept2024, 2nd commit

--- Fiddler- 27 Sept 2024.R
+++ Fiddler- 27 Sept 2024.R
@@ -24,10 +24,10 @@ RPSLS <- function(players=3) {
   
   for (p in (1:players)) {
     us <- throw[p]
-    them <- throw[-p] 
+    them <- throw[-p]
     rw <- which(elements == us)
     defeats <- key[rw, 2:3]
-    remain <- setdiff(them, defeats) 
+    remain <- setdiff(them, defeats)
     win <- length(remain) == 0
     if (win) {break}
   }
@@ -36,21 +36,33 @@ RPSLS <- function(players=3) {
 
 trials <- 10^7
 worker_cores <- detectCores() - 1
-run_set <- rep(3, trials)
 
 options(future.rng.onMisuse = "ignore")
-
-tic()
 plan(multisession, workers = worker_cores)
 
-  mc <- future_map_lgl(run_set, RPSLS)
-
-plan(sequential)
+# 3 Players
+run_set_3 <- rep(3, trials)
+tic()
+  mc_3 <- future_map_lgl(run_set_3, RPSLS)
 toc()
 
-win_perc <- sum(mc)/trials
-sprintf("%1.2f%%", win_perc*100)
+# 4 Players
+run_set_4 <- rep(4, trials)
+tic()
+  mc_4 <- future_map_lgl(run_set_4, RPSLS)
+toc()
+
+plan(sequential)
+
+
+win_perc_3 <- sum(mc_3)/trials
+sprintf("%1.2f%%", win_perc_3*100)
 # 48%
 
-rm(mc, run_set)
+win_perc_4 <- sum(mc_4)/trials
+sprintf("%1.2f%%", win_perc_4*100)
+# 25.6%
+
+rm(mc_3, mc_4)
+rm(run_set_3, run_set_4)
 rm(worker_cores)
