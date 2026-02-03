48dd0809b60c6d7af44cff438b0b6ca1a6b3b6a6
RNiederman <rob.niederman@trssllc.com>
2026-02-03 08:03:12 -0500

    23Jan26Fiddler, V4

--- Fiddler- 23 Jan 2026.R
+++ Fiddler- 23 Jan 2026.R
@@ -8,7 +8,7 @@ library(tictoc)
 Fiddler_Bingo <- function(rowws=3) {
   require(dplyr)
   
-  midpoint <- mean(1:rowws)
+  midpoint <- median(1:rowws)
   numbs <- rowws^2
   
   numb_set <- 1:numbs %>% sample
@@ -18,7 +18,7 @@ Fiddler_Bingo <- function(rowws=3) {
   bingo_grid[midpoint, midpoint] <- TRUE
   
   free_numb <- numb_grid[midpoint, midpoint]
-  numb_set2 <- numb_set[numb_set != free_numb] %>% sample
+  numb_set2 <- setdiff(numb_set, free_numb) %>% sample # Drop out the Number on the Free Square
   
   for (numb2 in numb_set2) {
     posn <- which(numb_grid == numb2)  
@@ -32,8 +32,7 @@ Fiddler_Bingo <- function(rowws=3) {
     m_test <- any(h_test, v_test, d_test1, d_test2)
     if (m_test) {break}
   }
-  
-  draws <- which(numb_set2 == numb2)
+  draws <- sum(bingo_grid) - 1 # -1 for the free square
   return(draws)
 }
 
@@ -41,7 +40,8 @@ Fiddler_Bingo <- function(rowws=3) {
 Histogram_Builder <- function(vect, bar_color=NULL) {
   if (is.null(bar_color)) {bar_color <- sample(colors(), 1)}
   
-  pt <- prop.table(table(vect));
+  tab <- table(vect)
+  pt <- prop.table(tab)
   high_y  <- max(pt) * 1.1
   bp <- barplot(pt,
           col = bar_color,
@@ -66,54 +66,70 @@ clusterExport(cl, "Fiddler_Bingo")
 tic.clearlog()
 
 tic("3x3 Bingo Card")
-  mc <- parSapply(cl, run_set, function(x) Fiddler_Bingo(3) )
+  mc_3 <- parSapply(cl, run_set, function(x) Fiddler_Bingo(3) )
 toc(log = TRUE)
 
 tic("5x5 Bingo Card")
-  xc_mc <- parSapply(cl, run_set, function(x) Fiddler_Bingo(5) )
+  mc_5 <- parSapply(cl, run_set, function(x) Fiddler_Bingo(5) )
 toc(log = TRUE)
 
 tic("7x7 Bingo Card")
-  xxc_mc <- parSapply(cl, run_set, function(x) Fiddler_Bingo(7) )
+  mc_7 <- parSapply(cl, run_set, function(x) Fiddler_Bingo(7) )
+toc(log = TRUE)
+
+tic("9x9 Bingo Card")
+  mc_9 <- parSapply(cl, run_set, function(x) Fiddler_Bingo(9) )
 toc(log = TRUE)
 
 stopCluster(cl)
 
+
 time_df <- tic.log(format = FALSE) %>% 
   do.call(rbind, .) %>% 
   as.data.frame %>% 
-  mutate(secs = as.numeric(toc) -  as.numeric(tic) ) %>% 
-  mutate(secs = floor(secs) ) %>% 
-  transmute(msg, time_delta = sprintf("%d:%02d", secs %/% 60, secs %% 60) )
+  mutate(time_delta = as.numeric(toc) -  as.numeric(tic) ) %>% 
+  mutate(s = as.integer(time_delta %% 60) ) %>% 
+  mutate(m = as.integer((time_delta %% 3600) %/% 60) ) %>% 
+  mutate(h = as.integer(time_delta %/% 3600) ) %>% 
+  transmute(msg, run_time = ifelse(h > 0,
+                                  sprintf("%d:%02d:%02d", h, m, s),
+                                  sprintf("%d:%02d", m, s) ) )
+  
+BAR_COLORS <- c("blue", "seagreen", "darkorange4", "magenta4")
 
-BAR_COLORS <- c("blue", "seagreen", "darkorange4")
-
-# Main Fiddler
-Histogram_Builder(mc, BAR_COLORS[1])
-soln <- sum(mc) / TRIALS
-sprintf("%.2f", soln)
+# Main Fiddler - 3x3 Bingo Card
+Histogram_Builder(mc_3, BAR_COLORS[1])
+soln_3 <- sum(mc_3) / TRIALS
+sprintf("%.2f", soln_3)
 # 3.47
 
-# Extra Credit
-Histogram_Builder(xc_mc, BAR_COLORS[2])
-xc_soln <- sum(xc_mc) / TRIALS
-sprintf("%.2f", xc_soln)
+# Extra Credit- 5x5 Bingo Card
+Histogram_Builder(mc_5, BAR_COLORS[2])
+soln_5 <- sum(mc_5) / TRIALS
+sprintf("%.2f", soln_5)
 # 13.61
 
-# Extra Extra Credit
-Histogram_Builder(xxc_mc, BAR_COLORS[3])
-xxc_soln <- sum(xxc_mc) / TRIALS
-sprintf("%.2f", xxc_soln)
+# 7x7 Bingo Card
+Histogram_Builder(mc_7, BAR_COLORS[3])
+soln_7 <- sum(mc_7) / TRIALS
+sprintf("%.2f", soln_7)
 # 30.63
+
+# 9x9 Bingo Card
+Histogram_Builder(mc_9, BAR_COLORS[4])
+soln_9 <- sum(mc_9) / TRIALS
+sprintf("%.2f", soln_9)
+# 54.88
 
 
 # Summaries
-summary(mc)
-summary(xc_mc)
-summary(xxc_mc)
+summary(mc_3)
+summary(mc_5)
+summary(mc_7)
+summary(mc_9)
 
 # Clean-Up
-# rm(ncores, cl, run_set, BAR_COLORS)
-# rm(list = ls(pattern = "mc"))
+rm(ncores, cl, run_set, BAR_COLORS)
+rm(list = ls(pattern = "mc_\\d+"))
 
 # https://thefiddler.substack.com/p/can-you-hop-in-a-spiral
